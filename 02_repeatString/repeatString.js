692b0ea1fe55d247735ab06b7f35c73ffd68aabb
Benher <199085821+Bencruz6@users.noreply.github.com>
2025-03-12 15:47:00 +0530

    did all the tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,25 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    let string='';
+    if(num<0){
+        return "ERROR";
+        
+    }
+    else{
+    while(num>0){
+        
+        string+=str;
+        // console.log(string);
+        num--;
+        
+    }
+    // console.log(string);
+    return string;
+}
+    
 };
+
+
+// repeatString("hey",5);
 
 // Do not edit below this line
 module.exports = repeatString;
